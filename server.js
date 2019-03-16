b6dea5195b5ed70e973b89f24dcbed44a9c019d1
Gonzalo <arecogonzalo@gmail.com>
2019-03-16 22:27:44 +0800

    starting mongoose

--- server.js
+++ server.js
@@ -1,37 +1,71 @@
-'use strict';
+const express = require("express");
+const mongo = require("mongodb");
+const mongoose = require("mongoose");
+const dns = require("dns");
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-var cors = require('cors');
+const cors = require("cors");
 
-var app = express();
+const app = express();
 
-// Basic Configuration 
-var port = process.env.PORT || 3000;
+require("dotenv").config();
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+// Basic Configuration
+const port = process.env.PORT || 3000;
+
+/** this project needs a db !! * */
+
+mongoose.connect(process.env.MONGOLAB_URI);
 
 app.use(cors());
 
-/** this project needs to parse POST bodies **/
+/** this project needs to parse POST bodies * */
 // you should mount the body-parser here
 
-app.use('/public', express.static(process.cwd() + '/public'));
+const bodyParser = require("body-parser");
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded()); // to support URL-encoded bodies
+
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", (req, res) => {
+  res.sendFile(`${process.cwd()}/views/index.html`);
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api/hello", (req, res) => {
+  res.json({ greeting: "hello API" });
 });
 
+// creacte Schema
 
-app.listen(port, function () {
-  console.log('Node.js listening ...');
+const urlSchema = new Schema({
+  original_url: String,
+  short_url: Number
+});
+
+const urlList = mongoose.model("urlList", urlSchema);
+
+app.post("/api/shorturl/new", (req, res) => {
+  const protocol = /^https?:\/\//i;
+
+  if (!protocol.test(req.body.url)) return res.json({ error: "invalid URL" });
+
+  const myUrl = new URL(req.body.url);
+
+  dns.lookup(myUrl.host, err => {
+    if (err) return res.json({ error: "invalid URL" });
+    return res.json(myUrl.host);
+  });
+});
+
+app.get("/api/shorturl/:id(\\d+)/", (req, res) => {
+  console.log(req.params);
+  res.send(req.params.id);
+});
+
+app.listen(port, () => {
+  console.log("Node.js listening ...");
 });
